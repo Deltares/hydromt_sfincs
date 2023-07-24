fc760e55cc2a836c5ecc5db8a38b42dac6fc90ed
roeldegoede <roeldegoede@outlook.com>
2023-07-24 12:06:49 +0200

    Fix replacing the mask values with one (due to dilating) by making a copy

--- hydromt_sfincs/subgrid.py
+++ hydromt_sfincs/subgrid.py
@@ -371,7 +371,7 @@ class SubgridTableRegular:
                 # calculate transform and shape of block at cell and subgrid level
                 da_mask_block = da_mask.isel(
                     {x_dim: slice(bm0, bm1), y_dim: slice(bn0, bn1)}
-                ).load()
+                ).copy().load()
                 check_block = np.all([s > 1 for s in da_mask_block.shape])
                 assert check_block, f"unexpected block shape {da_mask_block.shape}"
                 nactive = int(np.sum(da_mask_block > 0))
