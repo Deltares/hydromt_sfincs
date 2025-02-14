4d77323dd60fb3557049fb64b2666a579261a053
Leynse <Tim.Leijnse@deltares.nl>
2025-02-14 16:05:58 +0100

    - Add file infiltration.py for class SfincsInfiltration - Overview given of related original HydroMT-SFINCS functions, new to be added core functions, and DDB GUI focused additional functions - Assumed different infiltration types are always OR, so no subclasses needed

---
+++ hydromt_sfincs/infiltration.py
@@ -0,0 +1,54 @@
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pathlib import Path
+from typing import Union
+
+from hydromt.model.components import SpatialDatasetsComponent
+from hydromt_sfincs import SfincsModel
+from hydromt_sfincs import utils
+
+class SfincsInfiltration(SpatialDatasetsComponent):
+    def __init__(
+        self,        
+        model: SfincsModel,
+    ):
+        self._filename: None #FIXME - depends on type of infiltration
+        self._data: None #FIXME - depends on type of infiltration
+        super().__init__(model=model, 
+        )    
+
+    @property
+    def data(self) -> pd.GeoDataFrame: #FIXME
+        """Infiltration data.
+
+        Return FIXME
+        """
+        if self._data is None:
+            self._initialize()
+        return self._data
+    
+    # Original HydroMT-SFINCS setup_ functions:
+    # setup_constant_infiltration
+    # setup_cn_infiltration
+    # setup_cn_infiltration_with_ks
+    # To add: setup_green_ampt_infiltration
+    # To add: setup_horton_infiltration
+
+#%% core HydroMT-SFINCS functions:
+    # _initialize
+
+    # read
+    # write
+    # set
+    # create:
+        # create_constant_infiltration
+        # create_cn_infiltration
+        # create_cn_infiltration_with_ks
+        # create_green_ampt_infiltration
+        # create_horton_infiltration
+    # clear
+
+#%% DDB GUI focused additional functions:
+    # - yet unsupported in DDB-
