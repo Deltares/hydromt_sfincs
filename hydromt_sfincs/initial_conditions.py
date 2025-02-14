ec53612ab4688983a74a1bdd16df9dd0b55371dd
Leynse <Tim.Leijnse@deltares.nl>
2025-02-14 16:06:36 +0100

    - Add file initial_conditions.py for class SfincsInitialConditions - Overview given of related original HydroMT-SFINCS functions, new to be added core functions, and DDB GUI focused additional functions

---
+++ hydromt_sfincs/initial_conditions.py
@@ -0,0 +1,45 @@
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pathlib import Path
+from typing import Union
+
+from hydromt.model.components import SpatialDatasetsComponent
+from hydromt_sfincs import SfincsModel
+from hydromt_sfincs import utils
+
+class SfincsInitialConditions(SpatialDatasetsComponent):
+    def __init__(
+        self,        
+        model: SfincsModel,
+    ):
+        self._filename: str = "sfincs_ini.nc"
+        self._data: xr.Dataset = None
+        super().__init__(model=model, 
+        )    
+
+    @property
+    def data(self) -> xr.Dataset:
+        """Initial Conditions data.
+
+        Return xr.Dataset
+        """
+        if self._data is None:
+            self._initialize()
+        return self._data
+    
+    # Original HydroMT-SFINCS setup_ functions:
+    # not yet implemented
+
+#%% core HydroMT-SFINCS functions:
+    # _initialize
+    # read
+    # write
+    # set
+    # create
+    # clear
+
+#%% DDB GUI focused additional functions:
+    # interpolate
+    # interp2
