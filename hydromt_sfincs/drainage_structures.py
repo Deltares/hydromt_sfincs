239089778f5bd01052083689da62c1dbc2451274
Leynse <Tim.Leijnse@deltares.nl>
2025-02-14 16:05:08 +0100

    - Add file drainage_structure.py for class SfincsDrainageStructures - Overview given of related original HydroMT-SFINCS functions, new to be added core functions, and DDB GUI focused additional functions

---
+++ hydromt_sfincs/drainage_structures.py
@@ -0,0 +1,46 @@
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pathlib import Path
+from typing import Union
+
+from hydromt.model.components import ModelComponent
+from hydromt_sfincs import SfincsModel
+from hydromt_sfincs import utils
+
+class SfincsDrainageStructures(ModelComponent):
+    def __init__(
+        self,        
+        model: SfincsModel,
+    ):
+        self._filename: str = "sfincs.drain"
+        self._data: gpd.GeoDataFrame = None
+        super().__init__(model=model, 
+        )    
+
+    @property
+    def data(self) -> pd.GeoDataFrame:
+        """Drainage structures data.
+
+        Return geopandas.GeoDataFrame
+        """
+        if self._data is None:
+            self._initialize()
+        return self._data
+    
+#%% Original HydroMT-SFINCS setup_ functions:
+    #   setup_drainage_structures
+
+#%% core HydroMT-SFINCS functions:
+    # _initialize
+    # read
+    # write
+    # set
+    # create
+    # add
+    # delete
+    # clear
+
+#%% DDB GUI focused additional functions:
+    # - yet unsupported in DDB-
