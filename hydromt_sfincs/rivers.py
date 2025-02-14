f52a710baa3257f64e5e6bdbad00a9349d652275
Leynse <Tim.Leijnse@deltares.nl>
2025-02-14 17:00:53 +0100

    - Add file rivers.py for class SfincsRivers - Overview given of related original HydroMT-SFINCS functions, new to be added core functions, and DDB GUI focused additional functions

---
+++ hydromt_sfincs/rivers.py
@@ -0,0 +1,45 @@
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pathlib import Path
+from typing import Union, List
+
+from hydromt.model.components import ModelComponent
+from hydromt_sfincs import SfincsModel
+from hydromt_sfincs import utils
+
+class SfincsRivers(ModelComponent):
+    def __init__(
+        self,        
+        model: SfincsModel,
+    ):
+        # self._filename: str = "sfincs.src"
+        self._data: gpd.GeoDataFrame = None #FIXME - ?
+        super().__init__(model=model, 
+        )    
+
+    # @property FIXME - has no own data? or would that be mask/centerlines etc?
+    # def data(self) -> gpd.GeoDataFrame:
+    #     """Discharge points data.
+
+    #     Return gpd.GeoDataFrame
+    #     """
+    #     if self._data is None:
+    #         self._initialize()
+    #     return self._data
+    
+    # Original HydroMT-SFINCS setup_ functions:
+    # setup_river_inflow
+    # setup_river_outflow
+    # FIXME - also functions like burn in river???
+
+#%% core HydroMT-SFINCS functions:
+    # _initialize
+    # create:
+        # create_inflow
+        # create_outflow
+    # clear
+
+#%% DDB GUI focused additional functions:
+    # - yet unsupported in DDB-
