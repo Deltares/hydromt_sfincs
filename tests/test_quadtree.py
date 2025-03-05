8803f4ccaff41ccef05a79825535fbf2ede0cf0a
roeldegoede <roel.degoede@deltares.nl>
2025-03-05 15:51:26 +0100

    add tests; somehow qtr.read does seem to hold the files open now ..

--- tests/test_quadtree.py
+++ tests/test_quadtree.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from os.path import join, dirname, abspath
 import numpy as np
 import os
+from pathlib import Path
 from pyproj import CRS
 import pytest
 import shutil
+import xarray as xr
 import xugrid as xu
 
 from hydromt_sfincs import utils
@@ -12,11 +15,19 @@ from hydromt_sfincs.quadtree import QuadtreeGrid
 TESTDATADIR = join(dirname(abspath(__file__)), "data")
 
 
-def test_quadtree_io(tmpdir):
+def test_quadtree_io(tmp_dir):
+    # copy the test data to the tmp_path
+    fn = join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc")
+    fn_copy = tmp_dir.joinpath("sfincs.nc")
+
+    # Create file + copy
+    shutil.copy(fn, fn_copy)
+    print(fn, fn_copy)
+
     # Initialize a QuadtreeGrid object
     qtr = QuadtreeGrid()
     # Read a quadtree netcdf file
-    qtr.read(join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc"))
+    qtr.read(fn_copy)
     # Check the face coordinates
     face_coordinates = qtr.face_coordinates
     assert len(face_coordinates[0] == 4452)
@@ -28,8 +39,11 @@ def test_quadtree_io(tmpdir):
     assert crs == CRS.from_epsg(32633)
 
     # now write the quadtree to a new file
-    fn = tmpdir.join("sfincs_out.nc")
+    fn = join(tmp_dir, "sfincs_out.nc")
     qtr.write(fn)
+
+    # here we can still remove the file (but we dont want it)
+    # os.remove(fn)
 
     # read the new file and check the msk variable
     qtr2 = QuadtreeGrid()
@@ -41,18 +55,22 @@ def test_quadtree_io(tmpdir):
     # assert the dep variable is the same
     assert np.sum(qtr.data["dep"].values) == np.sum(qtr2.data["dep"].values)
 
+    # remove the files, why is this failing?
+    os.remove(fn)
+    os.remove(fn_copy)
 
-def test_overwrite_quadtree_nc(tmpdir):
-    ncfile = join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc")
-    nc_copy = join(str(tmpdir), "sfincs.nc")
+
+def test_overwrite_quadtree_nc(tmp_dir):
+    fn = join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc")
+    fn_copy = tmp_dir.joinpath("sfincs.nc")
 
     # Create file + copy
-    shutil.copy(ncfile, nc_copy)
+    shutil.copy(fn, fn_copy)
+    print(fn, fn_copy)
 
     # Open the copy with xu_open_dataset
     # This opens the file lazily
-    ds = utils.xu_open_dataset(nc_copy)
-    ds.close()
+    ds = utils.xu_open_dataset(fn_copy)
 
     # Convert to dataset
     ds = ds.ugrid.to_dataset()
@@ -60,41 +78,36 @@ def test_overwrite_quadtree_nc(tmpdir):
     # Try to write
     # NOTE this should fail because it still has lazy references to the file
     with pytest.raises(PermissionError):
-        ds.to_netcdf(nc_copy)
+        ds.to_netcdf(fn_copy)
 
     # Now perform the check and lazy loading check
-    utils.check_exists_and_lazy(ds, nc_copy)
+    utils.check_exists_and_lazy(ds, fn_copy)
 
     # Try to overwrite the file
-    ds.to_netcdf(nc_copy)
+    ds.to_netcdf(fn_copy)
 
     # Remove the copied file
-    os.remove(nc_copy)
+    os.remove(fn_copy)
 
 
-def test_overwrite_quadtree_nc_xu(tmpdir):
-    ncfile = join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc")
-    nc_copy = join(str(tmpdir), "sfincs.nc")
+def test_utils_open_dataset(tmp_dir):
+    # copy the test data to the tmp_path
+    fn = join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc")
+    fn_copy = tmp_dir.joinpath("sfincs.nc")
 
-    # Create file + copy
-    shutil.copy(ncfile, nc_copy)
+    shutil.copy(fn, fn_copy)
 
-    # Open the copy with xu_open_dataset
-    # This opens the file lazily
-    with xu.open_dataset(nc_copy) as ds:
-        # Convert to dataset
-        ds = ds.ugrid.to_dataset()
+    ds = utils.xu_open_dataset(fn_copy)
+    os.remove(fn_copy)
 
-    # Try to write
-    # NOTE this should fail because it still has lazy references to the file
-    with pytest.raises(PermissionError):
-        ds.to_netcdf(nc_copy)
 
-    # Now perform the check and lazy loading check
-    utils.check_exists_and_lazy(ds, nc_copy)
+def test_xu_open_dataset(tmp_dir):
+    # copy the test data to the tmp_path
+    fn = join(TESTDATADIR, "sfincs_test_quadtree", "sfincs.nc")
+    fn_copy = tmp_dir.joinpath("sfincs.nc")
 
-    # Try to overwrite the file
-    ds.to_netcdf(nc_copy)
+    shutil.copy(fn, fn_copy)
 
-    # Remove the copied file
-    os.remove(nc_copy)
+    ds = xu.open_dataset(fn_copy)
+    ds.close()
+    os.remove(fn_copy)
