0b6171343603eb4884ee40f67a7a90ea45b3646b
Roel de Goede <roel.degoede@deltares.nl>
2025-08-25 14:54:38 +0200

    update format yaml files for build

--- tests/test_1model_class.py
+++ tests/test_1model_class.py
@@ -454,14 +454,14 @@ def test_model_build(tmpdir, case):
 
     # Build model
     ini_fn = join(TESTDATADIR, _cases[case]["ini"])
-    steps = parse_config(ini_fn)
+    config = parse_config(ini_fn)
 
     # logger = setuplog(path=join(root, "hydromt.log"), log_level=10)
-    mod1 = SfincsModel(root=root, mode="w", **steps.pop("global", {}))
+    mod1 = SfincsModel(root=root, mode="w", **config.pop("global", {}))
     # convert steps to list of dicts
     # to be compatible with hydromt core Model.build()
-    steps = [{key: value} for key, value in steps.items()]
-    mod1.build(steps=steps)
+    # steps = [{key: value} for key, value in steps.items()]
+    mod1.build(steps=config.get("steps"))
     # Check if model is api compliant
     # non_compliant_list = mod1.test_model_api()
     # assert len(non_compliant_list) == 0
