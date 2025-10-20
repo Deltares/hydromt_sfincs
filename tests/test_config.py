bf74f03c06df8d2acbdbbf340c360c503581b422
Leynse <Tim.Leijnse@deltares.nl>
2025-10-20 15:53:29 +0200

    - Add checks for getting None var, and value var - Add check whether using write_description leads to a different sfincs.inp file

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,9 +12,15 @@ from hydromt_sfincs import SfincsModel
 def test_config_get_set(model_init):
     config = model_init.config
 
+    # check that a variable initiated as None is set correctly
+    assert config.get("mmax") == None
+
     # set a new value and get it
     config.set("mmax", 20)
     assert config.get("mmax") == 20
+
+    # check that another variable that has an actual variable is loaded correctly
+    assert config.get("advection") == 1    
 
     # set value out of bounds
     with pytest.raises(ValidationError):
@@ -32,7 +38,6 @@ def test_config_get_set(model_init):
     # Should raise KeyError for invalid attribute
     with pytest.raises(KeyError):
         config.set("invalid_key", 100)
-
 
 def test_config_io(tmp_path):
     # Start with model initialized with default values
@@ -66,6 +71,19 @@ def test_config_io(tmp_path):
     model1.config.read()
     assert model0.config.data == model1.config.data
 
+    # write config including descriptions
+    model1.config.write(filename= "sfincs_with_description.inp", write_description=True)
+
+    # read ascii file tmp_path/sfincs.inp
+    with open(os.path.join(tmp_path, "sfincs.inp"), 'r', encoding='ascii') as file:
+        # Read the contents of the file
+        contents = file.read()     
+    with open(os.path.join(tmp_path, "sfincs_with_description.inp"), 'r', encoding='ascii') as file:
+        # Read the contents of the file
+        contents1= file.read()
+
+    # Files should differ because of descriptions
+    assert contents != contents1
 
 def test_config_datetime(model_init):
     config = model_init.config
