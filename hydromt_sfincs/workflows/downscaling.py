7c7c267eb73e91d4a1ba76aa56cc65ff1fe0c3ee
Kees Nederhoff <kees.nederhoff@deltares.nl>
2026-04-22 12:46:23 -0700

    Update downscaling.py

--- hydromt_sfincs/workflows/downscaling.py
+++ hydromt_sfincs/workflows/downscaling.py
@@ -19,7 +19,7 @@ import hydromt
 from hydromt.data_catalog.drivers import RasterioDriver
 from hydromt.gis.gis_utils import zoom_to_overview_level
 
-from hydromt_sfincs.utils import build_overviews
+from hydromt_sfincs.utils import build_overviews, read_xy
 
 
 if TYPE_CHECKING:
